93bb37d86f6d5e63ba6804f38ed9fcb72ef87a42
0777204334 <127633805+0777204334@users.noreply.github.com>
2023-12-26 15:27:15 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function menuButton(){
+    document.getElementById("responsive").style.display="flex", transition="0.5s ease";
+}
+function closeButton(){
+    document.getElementById("responsive").style.display="none", transition="0.5s ease";
+}
